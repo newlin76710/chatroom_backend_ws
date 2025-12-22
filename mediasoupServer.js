17df82bdc532a649b6f96d27ee300d8c669c590f
newlin76710 <newlin76710@gmail.com>
2025-12-22 22:04:48 +0800

    Update mediasoupServer.js

--- mediasoupServer.js
+++ mediasoupServer.js
@@ -1,5 +1,14 @@
 import mediasoup from "mediasoup";
-
+async function getPublicIp() {
+  try {
+    const res = await fetch("https://api.ipify.org?format=json");
+    const data = await res.json();
+    return data.ip; // 公網 IP
+  } catch (err) {
+    console.error("Failed to get public IP, fallback to 127.0.0.1", err);
+    return "127.0.0.1";
+  }
+}
 let worker;
 let router;
 export const peers = {}; // { socketId: { transports: [], producers: [] } }
@@ -19,8 +28,9 @@ export function getRouter() {
 }
 
 export async function createWebRtcTransport() {
+  const publicIp = await getPublicIp();
   const transport = await router.createWebRtcTransport({
-    listenIps: [{ ip: "0.0.0.0", announcedIp: "127.0.0.1" }],
+    listenIps: [{ ip: "0.0.0.0", announcedIp: publicIp }],
     enableUdp: true,
     enableTcp: true,
     preferUdp: true
