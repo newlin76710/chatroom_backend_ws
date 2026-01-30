5382c377c668d1747abc738a26952d9b304d6b08
newlin76710 <newlin76710@gmail.com>
2026-01-30 18:41:43 +0800

    Update auth.js

--- auth.js
+++ auth.js
@@ -100,7 +100,7 @@ authRouter.post("/guest", async (req, res) => {
        ON CONFLICT (username) DO UPDATE SET
          last_login=EXCLUDED.last_login,
          is_online=true,
-         login_token=EXCLUDED.login_token
+         login_token=EXCLUDED.login_token,
          gender = EXCLUDED.gender
        RETURNING id, username, gender, level, exp`,
       [guestName, randomPassword, safeGender, now, guestToken]
