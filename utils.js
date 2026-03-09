cd357448c3792bca988be536d98eea57069b6ca8
newlin76710 <newlin76710@gmail.com>
2026-03-09 16:12:36 +0800

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,8 +1,7 @@
 const ANL = parseInt(process.env.ADMIN_MIN_LEVEL, 10) || 91;
 // 等級升級需求
 export function expForNextLevel(level) {
-  const MAX_LEVEL = AML-1;
+  const MAX_LEVEL = ANL-1;
   level = Math.min(level, MAX_LEVEL);
-
   return Math.floor(120 * level * level + 200);
 }
